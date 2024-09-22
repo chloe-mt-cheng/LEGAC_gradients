beff86894d0e056bfb7c6829aa180b9b49666388
chloe-mt-cheng <chloecheng@rocketmail.com>
2024-09-22 12:38:14 -0400

    Code to compute intrinsic model gradients

--- intrinsic_model.py
+++ intrinsic_model.py
@@ -42,7 +42,7 @@ from scipy.integrate import quad
 #Function flow
 # Read in LEGA-C catalogue for image parameters
 catalogue = pd.read_csv('intrinsic_model_cat.csv', header=0)
-LEGAC_cat_file = fits.open('/Users/chloecheng/Documents/LEGAC_resolved/legac_dr3_cat.fits')
+LEGAC_cat_file = fits.open('legac_dr3_cat.fits')
 LEGAC_cat_head = LEGAC_cat_file[1].header
 LEGAC_cat = LEGAC_cat_file[1].data
 LEGAC_cat_file.close()
@@ -83,11 +83,11 @@ def spectral_profile(target):
     
     #Import spectrum and weight map
     mask, obj = target.split('_')[0], target.split('_')[1]
-    spec2d_file = fits.open('/Users/chloecheng/Documents/LEGAC_resolved/2Dspectra/legac_' + mask + '_v3.11_spec2d_' + obj + '.fits')
+    spec2d_file = fits.open('/2Dspectra/legac_' + mask + '_v3.11_spec2d_' + obj + '.fits')
     flux2D = spec2d_file[0].data
     spec2d_file.close()
     
-    wht2d_file = fits.open('/Users/chloecheng/Documents/LEGAC_resolved/2Dspectra/legac_' + mask + '_v3.11_wht2d_' + obj + '.fits')
+    wht2d_file = fits.open('/2Dspectra/legac_' + mask + '_v3.11_wht2d_' + obj + '.fits')
     noise2D = wht2d_file[0].data
     wht2d_file.close()
     
@@ -202,7 +202,7 @@ def galfit_input(targ, directory, img_box, img_ctr, pixscale, output_name, input
 
     input_dict = dict(zip(keys, vals))
     input_df = pd.Series(input_dict, index=input_dict.keys())
-    input_df.to_csv('/Users/chloecheng/packages/galfit/%s/%s.input' %(directory, input_name), sep='\t', header=None)
+    input_df.to_csv('/galfit/%s/%s.input' %(directory, input_name), sep='\t', header=None)
     
 def galfit_model(directory, input_name):
     """Run galfit from OS and put the output files in a directory.
@@ -219,8 +219,8 @@ def galfit_model(directory, input_name):
     None
     """
 
-    os.system('/Users/chloecheng/packages/galfit/galfit /Users/chloecheng/packages/galfit/%s/%s.input >/dev/null 2>&1' %(directory, input_name))
-    os.system('mv /Users/chloecheng/Documents/LEGAC_resolved/*_galfit.fits /Users/chloecheng/Documents/LEGAC_resolved/galfit_imgs/%s/' %directory)
+    os.system('/galfit/galfit /galfit/%s/%s.input >/dev/null 2>&1' %(directory, input_name))
+    os.system('mv *_galfit.fits /galfit_imgs/%s/' %directory)
     
 def new_dimensions(n, m):
     """Return the new number of dimensions for a square grid when you divide n pixels into m subpixels.
